8a353fade7fa129057f940fd5eea378821e447df
1269941391 <1269941391@qq.com>
2017-09-27 10:39:48 +0800

    测试提交1

---
+++ Svc1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Svc1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
